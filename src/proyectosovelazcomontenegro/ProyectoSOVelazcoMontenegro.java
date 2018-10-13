cf7c4de6f11ade84b5c03ab61a529d16f1ec029a
YAlejandro Velazco <tim@timrich.org>
2018-10-13 12:50:26 -0400

    Commit inicial

---
+++ src/proyectosovelazcomontenegro/ProyectoSOVelazcoMontenegro.java
@@ -0,0 +1,12 @@
+
+package proyectosovelazcomontenegro;
+
+
+public class ProyectoSOVelazcoMontenegro {
+
+    
+    public static void main(String[] args) {
+      new GUI().setVisible(true);
+    }
+    
+}
